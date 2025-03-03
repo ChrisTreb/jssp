4f03ca441d649c2d803c0df6cd78c052a0efb37b
CTRL <christophe.trebaol@gmail.com>
2025-03-03 22:56:30 +0100

    Added syntax

---
+++ extension.js
@@ -0,0 +1,10 @@
+// extension.js
+function activate(_context) {
+    console.log('L’extension JSSP est activée !');
+}
+
+exports.activate = activate;
+
+function deactivate() { }
+
+exports.deactivate = deactivate;
